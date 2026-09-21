2132e8e8c3e2d598bfa5e90b60f3bcf59d3f11a0
jomansameer-collab <jomansameer@gmail.com>
2026-09-21 21:46:09 +0300

    upload m p

---
+++ data.js
@@ -0,0 +1,45 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-sora-pls",
+      "name": "sora pls",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 720,
+      "initialViewParameters": {
+        "yaw": 1.3964209250938087,
+        "pitch": 0,
+        "fov": 1.335888521808506
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 1.7502466541334112,
+          "pitch": 0.13004006524346678,
+          "title": "من الصحراء الى وطن",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "اليوم الوطني",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
